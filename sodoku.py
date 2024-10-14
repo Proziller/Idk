4124639d25c1876ce62572774fcea9f130d21680
Proziller <luke.stein.1712@gmail.com>
2024-10-14 13:21:12 +0200

    q

--- sodoku.py
+++ sodoku.py
@@ -1,10 +1,7 @@
 #!/bin/python3
 
-map1 = [
-    [{"zahl": "Hello", "writable": False},{"zahl": "world", "writable": False}],
-    [{"zahl": "", "writable": False},{"zahl": "", "writable": False}],
 ]
-map2 = [
+map = [
     [["Hello",False],["world",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False]],
     [[" ",False],["2nd row 2nd column",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False]],
     [[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False]],
@@ -23,19 +20,19 @@ pos2 = input("pos2: ")
 if pos1 in zahlen:
     pos1 = int(pos1)
     pos1 -= 1
+    if pos2 in zahlen:
+        pos2 = int(pos2)
+        pos2 -= 1
+        print(map[pos1][pos2][0])
+    else:
+        print("error")
 else:
     print("error")
 
-if pos2 in zahlen:
-    pos2 = int(pos2)
-    pos2 -= 1
-else:
-    print("error")
+
 
 #print(map1)
 #print(map2)
 
 #print(map1[0][0])
 #print("pos1: ", pos1, "pos2: ", pos2, "map2[0][0]:\n", map2[0][0])
-
-print(map2[pos1][pos2][0])
