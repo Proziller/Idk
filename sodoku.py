a45954e98cc73a1ac2a2c00ccd30ff26a830b3da
Proziller <luke.stein.1712@gmail.com>
2024-10-14 13:00:13 +0200

    initial commit

--- sodoku.py
+++ sodoku.py
@@ -1,1 +1,33 @@
 #!/bin/python3
+
+map1 = [
+    [{"zahl": "", "writable": False},{"zahl": "", "writable": False}],
+    [{"zahl": "", "writable": False},{"zahl": "", "writable": False}],
+]
+map2 = [
+    [[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False]],
+    [[" ",false],[" ",false],[" ",false],[" ",false],[" ",false],[" ",false],[" ",false],[" ",false],[" ",false]],
+    [[" ",false],[" ",false],[" ",false],[" ",false],[" ",false],[" ",false],[" ",false],[" ",false],[" ",false]],
+    [[" ",false],[" ",false],[" ",false],[" ",false],[" ",false],[" ",false],[" ",false],[" ",false],[" ",false]],
+    [[" ",false],[" ",false],[" ",false],[" ",false],[" ",false],[" ",false],[" ",false],[" ",false],[" ",false]],
+    [[" ",false],[" ",false],[" ",false],[" ",false],[" ",false],[" ",false],[" ",false],[" ",false],[" ",false]],
+    [[" ",false],[" ",false],[" ",false],[" ",false],[" ",false],[" ",false],[" ",false],[" ",false],[" ",false]],
+    [[" ",false],[" ",false],[" ",false],[" ",false],[" ",false],[" ",false],[" ",false],[" ",false],[" ",false]],
+    [[" ",false],[" ",false],[" ",false],[" ",false],[" ",false],[" ",false],[" ",false],[" ",false],[" ",false]]
+]
+zahlen = ["1","2","3","4","5","6","7","8","9"]
+
+pos1 = input("pos1: ")
+pos2 = input("pos2: ")
+
+if pos1 in zahlen:
+    pos1 = int(pos1)
+else:
+    print("error")
+
+if pos2 in zahlen:
+    pos2 = int(pos1)
+else:
+    print("error")
+
+print(map2[pos1][pos2][0])
