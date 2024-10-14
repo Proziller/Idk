87bb897a8283f0a9172a8a51d26e7f4915890c4d
Proziller <luke.stein.1712@gmail.com>
2024-10-14 13:33:01 +0200

    q

--- sodoku.py
+++ sodoku.py
@@ -1,8 +1,8 @@
 #!/bin/python3
 
 map = [
-    [["Hello",False],["world",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False]],
-    [[" ",False],["2nd row 2nd column",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False]],
+    [[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False]],
+    [[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False]],
     [[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False]],
     [[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False]],
     [[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False]],
@@ -11,24 +11,33 @@ map = [
     [[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False]],
     [[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False],[" ",False]]
 ]
-zahlen = ["1","2","3","4","5","6","7","8","9"]
+def mark():
+    zahlen = ["1","2","3","4","5","6","7","8","9"]
 
-pos1 = input("pos1: ")
-pos2 = input("pos2: ")
+    pos1 = input("pos1: ")
 
-if pos1 in zahlen:
-    pos1 = int(pos1)
-    pos1 -= 1
-    if pos2 in zahlen:
-        pos2 = int(pos2)
-        pos2 -= 1
-        print(map[pos1][pos2][0])
+    if pos1 in zahlen:
+        pos1 = int(pos1)
+        pos1 -= 1
+
+        pos2 = input("pos2: ")
+
+        if pos2 in zahlen:
+            pos2 = int(pos2)
+            pos2 -= 1
+            if map[pos1][pos2][1] == False:
+                new_num = input("new entry")
+
+                if new_num in zahlen:
+                    map[pos1][pos2][0] = new_num
+                else:
+                    print("error")
+            else:
+                print("already occupied")
+        else:
+            print("error")
     else:
         print("error")
-else:
-    print("error")
-
-
 
 #print(map1)
 #print(map2)
